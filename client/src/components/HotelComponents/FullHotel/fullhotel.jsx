068be7a0e4d8732a8ec3f4f5ed8b764b78729b00
thu <165041915+ThuPham60232004@users.noreply.github.com>
2024-10-22 13:31:57 +0700

    THU

--- client/src/components/HotelComponents/FullHotel/fullhotel.jsx
+++ client/src/components/HotelComponents/FullHotel/fullhotel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetch from "../../../hooks/useFetch";
 import "./fullhotel.css";
 import { useNavigate } from "react-router-dom";
@@ -7,19 +7,59 @@ const FullHotel = () => {
   const { data, loading, error } = useFetch("http://localhost:9000/api/hotels/all");
   const navigate = useNavigate();
 
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedName, setSelectedName] = useState("");
+
   const handleNavigate = (id) => {
     navigate(`/hotels/${id}`);
   };
 
+  const handleCityChange = (e) => {
+    setSelectedCity(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setSelectedName(e.target.value);
+  };
+  const cities = [...new Set(data.map((item) => item.city))];
+  const names = [...new Set(data.map((item) => item.type))];
+
+  const filteredHotels = data.filter((item) => {
+    return (
+      (selectedCity === "" || item.city === selectedCity) &&
+      (selectedName === "" || item.type === selectedName)
+    );
+  });
+
   if (error) return <div>Lỗi tải dữ liệu</div>;
 
   return (
-    <div className="featured1">
+  <div className='FullHotelContainer'>
+     <div className="filters">
+        <select value={selectedCity} onChange={handleCityChange} className="filterSelect">
+          <option value="">Tất cả thành phố</option>
+          {cities.map((city) => (
+            <option key={city} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+
+        <select value={selectedName} onChange={handleNameChange} className="filterSelect">
+          <option value="">Tất cả loại khách sạn</option>
+          {names.map((type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+      </div>
+        <div className="featured1">
       {loading ? (
         "Loading"
       ) : (
         <>
-          {data.map((item) => (
+          {filteredHotels.map((item) => (
             <div className="featuredItem1" key={item._id} onClick={() => handleNavigate(item._id)}>
               <img
                 src={item.photos[0]}
@@ -42,6 +82,7 @@ const FullHotel = () => {
         </>
       )}
     </div>
+</div>
   );
 };
 
